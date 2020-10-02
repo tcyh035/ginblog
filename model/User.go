b552932e69796722a9f4230cd4ac4fb8f62287e6
yh <jsyh_1123@163.com>
2020-10-02 12:48:01 +0800

    modify password length

--- model/User.go
+++ model/User.go
@@ -11,7 +11,7 @@ import (
 type User struct {
 	gorm.Model
 	Username string `gorm:"type:varchar(20);not null " json:"username"`
-	Password string `gorm:"type:varchar(50);not null " json:"password"`
+	Password string `gorm:"type:varchar(200);not null " json:"password"`
 	Role     int    `gorm:"type:int " json:"role"`
 }
 
